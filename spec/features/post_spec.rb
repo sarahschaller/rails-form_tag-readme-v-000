510a86e93a4a693b61f9c1ae192fc2dc0985d55d
Sarah Schaller <sarcsch@aol.com>
2017-10-29 23:12:35 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'new post' do
-  it 'ensures that the form route works with the /new action' do 
+  it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
