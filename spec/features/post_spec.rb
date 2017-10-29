1067ab7de9ad555b6502023babc66f4333af5e45
Sarah Schaller <sarcsch@aol.com>
2017-10-29 23:14:31 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,7 +6,7 @@ describe 'new post' do
     expect(page.status_code).to eq(200)
   end
 
-  it 'renders HTML in the /new template' do 
+  it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
   end
